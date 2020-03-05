c81133c455ee6b3eab83bd61694da1af918cc093
Thomas Refis <thomas.refis@gmail.com>
2020-03-05 19:51:22 +0100

    cmdliner the thing, add some options

--- main.ml
+++ main.ml
@@ -1,19 +1,39 @@
-let () =
-  let fn = Sys.argv.(1) in
-  let width =
-    try int_of_string (Sys.argv.(2))
-    with _ -> 80
-  in
+let fmt_file fn =
   let ic = open_in fn in
   let b = Lexing.from_channel ic in
-  let doc =
     if Filename.check_suffix fn "mli" then
       let sg = Parse_source.interface b in
       Print_source.Signature.pp sg
     else
       let st = Parse_source.implementation b in
       Print_source.Structure.pp st
-  in
-  PPrint.ToChannel.pretty 10. width stdout doc;
-  print_newline ();
-  flush stdout
+
+open Cmdliner
+
+let (let+) x f = Term.app (Term.const f) x
+let (and+) t1 t2 = Term.(const (fun x y -> (x, y)) $ t1 $ t2)
+
+let (let*) = Result.bind
+
+let cmd =
+  let open Options in
+  let+ record_exp = Record.expression_arg
+  and+ record_pat = Record.pattern_arg
+  and+ record_all = Record.all_arg
+  and+ width = Arg.(value & opt int 80 & info ["width"])
+  and+ files = Arg.(value & pos_all file [] & info ~doc:"files to format" []) in
+  let* () = Choice.setopts Record.[ expression_choice ] record_exp in
+  let* () = Choice.setopts Record.[ pattern_choice ] record_pat in
+  let* () = Choice.setopts Record.[ expression_choice; pattern_choice ] record_all in
+  List.iter (fun fn ->
+    let doc = fmt_file fn in
+    PPrint.ToChannel.pretty 10. width stdout doc;
+    print_newline ();
+  ) files;
+  Ok (flush stdout)
+
+let info =
+  Term.info "pp"
+
+let () =
+  Term.exit (Term.eval (cmd, info))
---
+++ options.ml
@@ -0,0 +1,81 @@
+module Choice : sig
+  type 'a t
+
+  type err = Choice_already_made
+
+  val mint : string -> default:'a -> 'a t
+
+(*   val run : 'a t list -> 'a option Term.t -> unit Term.t *)
+  val setopts : 'a t list -> 'a option -> (unit, [ `Msg of string ]) result
+end = struct
+  type 'a t = {
+    mutable user: 'a option;
+    name: string;
+    default: 'a;
+  }
+
+  let mint name ~default = { name; default; user = None }
+
+  type err = Choice_already_made
+
+  let set t choice =
+    if Option.is_some t.user then
+      Error (`Msg (Printf.sprintf "overlapping flags for %s" t.name))
+    else
+      Ok (t.user <- Some choice)
+
+  let setopt t = function
+    | None -> Ok ()
+    | Some x -> set t x
+
+  let rec iter_m ~f = function
+    | [] -> Ok ()
+    | x :: xs -> Result.bind (f x) (fun () -> iter_m ~f xs)
+
+  let setopts ts choice = iter_m ~f:(fun t -> setopt t choice) ts
+end
+
+module Dockable = struct
+  type t = Fit_or_vertical | Docked
+
+  let parse = function
+    | "fit-or-vertical" -> Ok (Some Fit_or_vertical)
+    | "docked" -> Ok (Some Docked)
+    | s ->
+      let msg = Printf.sprintf "accepted fit-or-vertical | docked, got %S" s in
+      Error (`Msg msg)
+
+  let print ppf t =
+    Format.pp_print_string ppf
+      (match t with
+       | Some Fit_or_vertical -> "fit-or-vertical"
+       | Some Docked -> "docked"
+       | _ -> "default")
+
+  let t = Cmdliner.Arg.conv (parse, print)
+end
+
+module Record = struct
+  let expression_choice =
+    Choice.mint "record expressions" ~default:Dockable.Fit_or_vertical
+
+  let pattern_choice =
+    Choice.mint "record patterns" ~default:Dockable.Docked
+
+  open Cmdliner
+
+  let expression_arg =
+    let open Arg in
+    let info = info ~doc:"formatting of record expressions" ["record-expr"] in
+    value & opt Dockable.t None info
+
+  let pattern_arg =
+    let open Arg in
+    let info = info ~doc:"formatting of record patterns" ["record-patt"] in
+    value & opt Dockable.t None info
+
+  let all_arg =
+    let open Arg in
+    let info = info ~doc:"formatting of records (in all contexts)" ["record"] in
+    value & opt Dockable.t None info
+end
